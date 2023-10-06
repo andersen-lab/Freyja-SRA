53495d99895281115ae4ab9b91b83f8e52471dab
Dylan Pilz <dpilz@login00.scripps.edu>
2023-10-06 10:23:40 -0700

    deploy automated workflow

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+set -x
 nextflow run main.nf \
     --input data/new_samples.csv \
-    --num_samples 10 \
-    -profile docker \
+    --num_samples 5 \
+    -profile singularity \
     -entry fetch_sra
 
 # Clean up
