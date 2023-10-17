87e2c92be270cce2f356b656c806a86ee11a0bfe
dylanpilz <dpilz@ucsd.edu>
2023-10-17 12:24:59 -0700

    add looping to run_pipeline 100 samples

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -x
-nextflow run main.nf \
-    --input data/new_samples.csv \
-    --num_samples 10 \
-    -profile docker \
-    -entry fetch_sra
 
-# Clean up
-rm -rf work
+for i in {1..10}
+do
+    nextflow run main.nf \
+        --input data/new_samples.csv \
+        --num_samples 10 \
+        -profile docker \
+        -entry fetch_sra &
+    BACK_PID=$!
+    wait $BACK_PID
+    rm -rf work
+
+    # Remove the first 10 lines from the input file
+    sed -i '1,10d' data/new_samples.csv
+done
