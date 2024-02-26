02b95b2ba767539001b7bb062e6cf357f93d2a64
dylanpilz <dpilz@ucsd.edu>
2024-02-26 13:27:43 -0800

    concat aggregate files locally instead of gcloud compose

---
+++ scripts/concat_agg_files.py
@@ -0,0 +1,10 @@
+import os
+
+files = ['demix', 'variants', 'metadata']
+
+for file in files:
+    with open(f'outputs/aggregate/aggregate_{file}.json', 'w') as outfile:
+        with open(f'outputs/aggregate/aggregate_{file}_new.json', 'r') as infile:
+            outfile.write(infile.read())
+
+    os.remove(f'outputs/aggregate/aggregate_{file}_new.json')
