ab52dc689ff377ebe33374dd0ba35195666f816a
dylanpilz <dpilz@ucsd.edu>
2024-02-26 14:14:28 -0800

    append to existing file

--- scripts/concat_agg_files.py
+++ scripts/concat_agg_files.py
@@ -3,7 +3,7 @@ import os
 files = ['demix', 'variants', 'metadata']
 
 for file in files:
-    with open(f'outputs/aggregate/aggregate_{file}.json', 'w') as outfile:
+    with open(f'outputs/aggregate/aggregate_{file}.json', 'a') as outfile:
         with open(f'outputs/aggregate/aggregate_{file}_new.json', 'r') as infile:
             outfile.write(infile.read())
 
