d7bff85e2cf2269d71204d8b57055f30ec20e0c6
dylanpilz <dpilz@ucsd.edu>
2024-03-15 11:22:01 -0700

    remove duplicates from aggregate data before pushing

--- scripts/concat_agg_files.py
+++ scripts/concat_agg_files.py
@@ -7,4 +7,7 @@ for file in files:
         with open(f'outputs/aggregate/aggregate_{file}_new.json', 'r') as infile:
             outfile.write(infile.read())
 
+    os.system(f'awk \'!seen[$0]++\' outputs/aggregate/aggregate_{file}.json > outputs/aggregate/aggregate_{file}_dedup.json')        
+    os.remove(f'outputs/aggregate/aggregate_{file}.json')
     os.remove(f'outputs/aggregate/aggregate_{file}_new.json')
+    os.rename(f'outputs/aggregate/aggregate_{file}_dedup.json', f'outputs/aggregate/aggregate_{file}.json')
