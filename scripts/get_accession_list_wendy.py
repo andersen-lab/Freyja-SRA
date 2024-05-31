c2cfb5efd14a60f63c0a3e6044aafb8cac9a6972
dylanpilz <dpilz@ucsd.edu>
2024-05-31 14:26:35 -0700

    fix accession list

--- scripts/get_accession_list_wendy.py
+++ scripts/get_accession_list_wendy.py
@@ -4,6 +4,7 @@ import pandas as pd
 batch_size = int(sys.argv[1])
 metadata = pd.read_csv('data/all_metadata.csv')
 
+metadata = metadata[metadata['sample_status'] == 'to_run'] 
 accessions = metadata['accession'][-1 * batch_size:] # Get last batch_size accessions
 
 accessions.to_csv('data/accession_list.csv', index=False, header=False)
