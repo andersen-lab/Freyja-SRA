782057448028e1d0073cf60b8cd02e5812762c13
dylanpilz <dpilz@ucsd.edu>
2024-05-31 08:25:12 -0700

    run remaining samples

--- scripts/get_accession_list_wendy.py
+++ scripts/get_accession_list_wendy.py
@@ -4,13 +4,6 @@ import pandas as pd
 batch_size = int(sys.argv[1])
 metadata = pd.read_csv('data/all_metadata.csv')
 
-## Temporary: Restrict to Freyja-global period (USA, 04-01-2022 - 10-01-2023)
-metadata = metadata[metadata['collection_date'] >= '2022-04-01']
-metadata = metadata[metadata['collection_date'] < '2023-10-01']
-metadata = metadata[metadata['geo_loc_name'].str.contains('USA', na=False)]
-######################################################################
-
-metadata = metadata[metadata['sample_status'] == 'fastq_error']
 accessions = metadata['accession'][-1 * batch_size:] # Get last batch_size accessions
 
 accessions.to_csv('data/accession_list.csv', index=False, header=False)
