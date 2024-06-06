be0e8abe32a49a64e52056212b5e680a0f28bdf4
dylanpilz <dylan.pilz@live.com>
2024-06-06 13:17:44 -0700

    fix: aggregate_demix_by_week

--- scripts/aggregate_demix_by_week.py
+++ scripts/aggregate_demix_by_week.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 from epiweeks import Week
 import datetime
 
@@ -11,9 +12,7 @@ demix = demix[demix['sra_accession'].isin(metadata['sra_accession'])]
 
 
 # Remove samples where the sum of the lineage prevalence is greater than 1
-print('accessions_before', demix['sra_accession'].nunique())
 demix = demix[~demix.groupby('sra_accession')['prevalence'].transform('sum').gt(1)]
-print('accessions_after', demix['sra_accession'].nunique())
 
 # Add metadata to demix
 df_agg = pd.merge(demix, metadata, on='sra_accession', how='left')
@@ -28,34 +27,36 @@ df_agg['pop_weighted_prevalence'] = df_agg['prevalence'] * df_agg['ww_population
 df_agg['collection_date'] = pd.to_datetime(df_agg['collection_date'])
 df_agg['epiweek'] = df_agg['collection_date'].apply(lambda x: Week.fromdate(x))
 
-df_agg_weekly = df_agg.groupby(['epiweek', 'name', 'geo_loc_region']).agg({
+# Create a dictionary mapping epiweek and region to the total prevalence
+total_lineage_prevalence = df_agg.groupby(['epiweek', 'geo_loc_region']).agg({
     'pop_weighted_prevalence': 'sum',
-    'ww_population': 'sum',
-    'sra_accession': 'nunique',
-    'collection_date': 'first',
-    'collection_site_id': 'nunique'
-}).reset_index().rename(columns={
-    'sra_accession': 'num_samples',
-    'ww_population': 'total_population',
-    'collection_site_id': 'num_collection_sites'
-    })
+}).reset_index()
 
-# get mean prevalence for each lineage
-df_agg_weekly['mean_lineage_prevalence'] = df_agg_weekly['pop_weighted_prevalence'] / df_agg_weekly['total_population']
+total_prev_dict = {total_lineage_prevalence['epiweek'].astype(str)[i] + '_' + total_lineage_prevalence['geo_loc_region'][i]: total_lineage_prevalence['pop_weighted_prevalence'][i] for i in range(len(total_lineage_prevalence))}
+
+df_agg_weekly = df_agg.groupby(['epiweek', 'geo_loc_region', 'name']).agg({
+    'pop_weighted_prevalence': 'sum',
+    'collection_site_id': 'nunique',
+    'sra_accession': 'nunique',
+}).reset_index().rename(columns={
+    'collection_site_id': 'num_sites',
+    'sra_accession': 'num_samples',
+})
+
+df_agg_weekly['id'] = df_agg_weekly['epiweek'].astype(str) + '_' + df_agg_weekly['geo_loc_region']
+df_agg_weekly['total_lineage_prevalence'] = df_agg_weekly['id'].map(total_prev_dict)
+df_agg_weekly['mean_lineage_prevalence'] = df_agg_weekly['pop_weighted_prevalence'] / df_agg_weekly['total_lineage_prevalence']
+
+
+df_agg_weekly['id'] = df_agg_weekly['id'] + '_' + df_agg_weekly['name']
 
 
 df_agg_weekly['week_start'] = df_agg_weekly['epiweek'].apply(lambda x: x.startdate()).astype(str)
 df_agg_weekly['week_end'] = df_agg_weekly['epiweek'].apply(lambda x: x.enddate()).astype(str)
 
-df_agg_weekly = df_agg_weekly[['epiweek','week_start', 'week_end', 'total_population', 'num_collection_sites', 'num_samples', 'geo_loc_region', 'name', 'mean_lineage_prevalence']]
-df_agg_weekly['id'] = df_agg_weekly['epiweek'].astype(str) + '_' + df_agg_weekly['geo_loc_region'].str.replace(' ', '_') + '_' + df_agg_weekly['name']
+df_agg_weekly = df_agg_weekly[['id', 'epiweek', 'week_start', 'week_end', 'geo_loc_region','num_sites', 'num_samples', 'name', 'mean_lineage_prevalence']]
 
-
-# Get number of duplicate ids
-print('Number of duplicate ids:', df_agg_weekly['id'].duplicated().sum())
-
-# Check that the prevalence for each state and week sums to 1
-print('Sum of prevalence:', df_agg_weekly.groupby(['epiweek', 'geo_loc_region'])['mean_lineage_prevalence'].sum())
-
-print(df_agg_weekly)
-df_agg_weekly.to_json('outputs/aggregate/aggregate_demix_weekly.json', orient='records', lines=True)
+df_agg_weekly.to_csv('outputs/aggregate/aggregate_demix_by_week.csv', index=False)
+df_out = pd.read_csv('outputs/aggregate/aggregate_demix_by_week.csv')
+os.remove('outputs/aggregate/aggregate_demix_by_week.csv')
+df_out.to_json('outputs/aggregate/aggregate_demix_weekly.json', orient='records', lines=True)
