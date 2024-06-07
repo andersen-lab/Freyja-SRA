e8afb3e2448752c75b1969965349ead4a9271e42
dylanpilz <dpilz@ucsd.edu>
2024-06-07 09:22:57 -0700

    handle missing variants output for demix rerun

--- scripts/get_variants_output.sh
+++ scripts/get_variants_output.sh
@@ -1,6 +1,11 @@
 # for accession in data/samples_to_rerun.txt, copy {accession}.variants.tsv and {accession}.depths.tsv demix_rerun/
 
 while read -r accession; do
+    gsutil stat "gs://outbreak-ww-data/variants/${accession}.variants.tsv"
+    if [ $? -ne 0 ]; then
+        echo "gs://outbreak-ww-data/variants/${accession}.variants.tsv does not exist"
+        continue
+    fi
     gcloud storage cp gs://outbreak-ww-data/variants/${accession}.variants.tsv" "demix_rerun/"
     gcloud storage cp gs://outbreak-ww-data/variants/${accession}.depths.tsv" "demix_rerun/"
 done < "data/samples_to_rerun.txt"
