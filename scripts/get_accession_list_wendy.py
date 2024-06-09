23b994ecf6d4d334744c68aaeb1e3ab7da0f6222
dylanpilz <dpilz@ucsd.edu>
2024-06-09 11:45:38 -0700

    run all samples

--- scripts/get_accession_list_wendy.py
+++ scripts/get_accession_list_wendy.py
@@ -5,9 +5,7 @@ batch_size = int(sys.argv[1])
 metadata = pd.read_csv('data/all_metadata.csv')
 
 metadata = metadata[metadata['sample_status'] == 'to_run'] 
-metadata = metadata[metadata['geo_loc_name'].str.contains('USA', case=False)]
-metadata = metadata[metadata['collection_date'] >= '2022-04-01']
-metadata = metadata[metadata['collection_date'] <= '2023-10-01']
-accessions = metadata['accession'][:batch_size] # Get last batch_size accessions
+
+accessions = metadata['accession'][:batch_size] # Get first batch_size accessions
 
 accessions.to_csv('data/accession_list.csv', index=False, header=False)
