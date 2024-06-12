298777d4042e61243c39fb19fd3ec6e692ff6751
dylanpilz <dpilz@ucsd.edu>
2024-06-12 08:32:29 -0400

    syntax fix

--- scripts/get_variants_output.sh
+++ scripts/get_variants_output.sh
@@ -1,7 +1,7 @@
 # for accession in data/samples_to_rerun.txt, copy {accession}.variants.tsv and {accession}.depths.tsv demix_rerun/
 
 while read -r accession; do
-    gsutil stat "gs://outbreak-ww-data/variants/${accession}.variants.tsv"
+    gsutil stat gs://outbreak-ww-data/variants/${accession}.variants.tsv
     if [ $? -ne 0 ]; then
         echo "gs://outbreak-ww-data/variants/${accession}.variants.tsv does not exist"
         continue
