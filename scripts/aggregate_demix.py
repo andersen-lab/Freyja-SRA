f4d2740619999545878ccbec571a2c79e5f18795
dylanpilz <dylan.pilz@live.com>
2024-09-11 05:46:48 -0700

    fix lineage crumbs

--- scripts/aggregate_demix.py
+++ scripts/aggregate_demix.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import yaml
 import numpy as np
 import pandas as pd
 from datetime import datetime
@@ -48,29 +47,6 @@ def merge_collapsed(df_agg):
     return df_agg
 
 
-def get_alias_key(lineages_yml='data/lineages.yml'):
-    with open(lineages_yml, 'r') as alias_key:
-        lineage_key = yaml.load(alias_key, Loader=yaml.Loader)
-    alias_key = dict([(lin['name'], lin['parent']) for lin in lineage_key if 'parent' in lin])
-    alias_key.update([(lin['name'], lin['alias']) for lin in lineage_key if lin['name'] != lin['alias']])
-    alias_key.update([r for lin in lineage_key for r in \
-        [(lin['name'], lin['name'].split('.')[0]), (lin['name'].split('.')[0], lin['alias'])] \
-        if (lin['name'] != lin['alias']) and len(lin['name'].split('.')) == 2 ])
-    for n in range(4):
-        alias_key.update([(alias, '.'.join(alias.split('.')[:-1])) for name, alias in alias_key.items() if not alias in alias_key and len(alias.split('.')) > 1])
-    alias_key.update({'A.1': 'A', 'B.1': 'B'})
-    return alias_key
-
-
-def _crumbs(lin, alias_key):
-    return [lin] + ( _crumbs(alias_key[lin], alias_key) if lin in alias_key else [])
-
-
-def crumbs(lin, alias_key):
-    lin = lin.upper()
-    return _crumbs(lin, alias_key) if lin in alias_key else crumbs(lin[:-1], alias_key) if len(lin.split('.')) > 1 else []
-
-
 def process_depth_file(acc):
     SPIKE_START = 21563
     SPIKE_END = 25384
@@ -109,8 +85,8 @@ def main():
     df_agg = df_agg[df_agg['coverage'].astype(float) > 0.0]
     
     # Get lineage breadcrumbs
-    alias_key = get_alias_key()
-    df_agg['crumbs'] = df_agg['name'].apply(lambda x: ';' + ';'.join(crumbs(x, alias_key)[::-1]) + ';')
+    alias_key = crumbs.get_alias_key('data/lineages.yml')
+    df_agg['crumbs'] = df_agg['name'].apply(lambda x: ';' + ';'.join(crumbs.crumbs(x, alias_key)[::-1]) + ';')
 
     # Get spike coverage
     df_agg = get_spike_coverage(df_agg)
